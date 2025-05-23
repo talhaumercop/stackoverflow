49245fe9e09b5a8dda4f7aa605962d96ec83025c
Talha Umar <talhaumar097@gmail.com>
2025-05-23 09:19:55 +0300

    added delete

--- src/app/api/answer/route.ts
+++ src/app/api/answer/route.ts
@@ -4,6 +4,7 @@ import { UserPreferences } from "@/ZustandStore/Auth";
 import { NextRequest, NextResponse } from "next/server";
 import { ID } from "node-appwrite";
 
+/// This is the route for creating an answer
 export async function POST(request:NextRequest){
     try {
         const {questionId,answer,authorId}=await request.json()
@@ -30,6 +31,40 @@ export async function POST(request:NextRequest){
     } catch (error:any) {
         return NextResponse.json({
             error:error?.message || "error creating the answer"
+        },{status:500})
+    }
+}
+
+/// This is the route for deleting an answer
+export async function DELETE(request:NextRequest){
+    try {
+        const {answerId}=await request.json()
+        if(!answerId ){
+            return NextResponse.json({
+                error:"missing required fields"
+            },{status:400})
+        }
+        const answer= await databases.getDocument(db,answerCollection,answerId)
+        const authorId=answer.author
+        if(!answer || !authorId){
+            return NextResponse.json({
+                error:"answer not found"
+            },{status:404})
+        }
+        const response= await databases.deleteDocument(db,answerCollection,answerId)
+        const prefs= await users.getPrefs<UserPreferences>(authorId)
+        await users.updatePrefs<UserPreferences>(authorId, {
+            reputation:Number(prefs.reputation)-1
+        })
+
+        return NextResponse.json({
+            success:true,
+            message:"answer deleted successfully",
+            data:response
+        },{status:200})
+    } catch (error:any) {
+        return NextResponse.json({
+            error:error?.message || "error deleting the answer"
         },{status:500})
     }
 }
