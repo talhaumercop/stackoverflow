d925b4ccd0e99d1f74d6f27e473ab415aaed4ecd
Talha Umar <talhaumar097@gmail.com>
2025-05-24 09:09:30 +0300

    continued vote

--- src/app/api/vote/route.ts
+++ src/app/api/vote/route.ts
@@ -1,4 +1,4 @@
-import {db, votesCollection } from "@/models/name";
+import {answerCollection, db, questionCollection, votesCollection } from "@/models/name";
 import { databases } from "@/models/server/config";
 import { NextRequest,NextResponse } from "next/server";
 import { Query } from "node-appwrite";
@@ -17,8 +17,34 @@ export async function POST(request: NextRequest) {
             Query.equal("votedById", votedById),
             Query.equal("type", type)
         ])
-        if(response.documents.length>0){}
+        if(response.documents.length>0){
+            await databases.deleteDocument(db,votesCollection,response.documents[0].$id)
+            const questionORanswer= await databases.getDocument(db,type==="question"?questionCollection:answerCollection,typeId)
+        }
         if(response.documents[0].voteStatus !== voteStatus){}
+        const [upvote,downvote]= await Promise.all([
+            databases.listDocuments(db,votesCollection,[
+              Query.equal("typeId", typeId),
+              Query.equal("votedById", votedById),
+              Query.equal("voteStatus","upvote")  
+            ]),
+            databases.listDocuments(db,votesCollection,[
+                Query.equal("typeId", typeId),
+                Query.equal("votedById", votedById),
+                Query.equal("voteStatus","downvote")
+            ])
+        ])
+        return NextResponse.json(
+        {
+            data:{
+                documents:null,
+                voteStatus:upvote.total = downvote.total
+            }
+        },
+        {
+            status: 200
+        }
+    )
     } catch (error:any) {
         return NextResponse.json({
             error: error.message || "error in vote",
